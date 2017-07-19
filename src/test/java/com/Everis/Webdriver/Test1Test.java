9310c91f1e73a760c9a9eece4a592e5299557a99
TestBCI <jesu.r.d.f@gmail.com>
2017-07-19 10:16:42 -0400

    creando testjunit

---
+++ src/test/java/com/Everis/Webdriver/Test1Test.java
@@ -0,0 +1,14 @@
+package com.Everis.Webdriver;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test1Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
